1362ddde789386a0a1a5febe51f808e6c32c5b9c
Frank Nicolas Herran Garzon <nicolasherrang98@gmail.com>
2020-09-27 10:51:57 -0500

    Se crea sección repaso con sus respectivos botones

--- icfesPreU/src/app/app-routing.module.ts
+++ icfesPreU/src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'review',
+    loadChildren: () => import('./review/review.module').then( m => m.ReviewPageModule)
+  },
 ];
 
 @NgModule({
