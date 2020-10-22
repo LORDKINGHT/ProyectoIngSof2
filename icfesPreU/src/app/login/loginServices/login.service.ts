4c5bffb5f0f688fce76520f1fae393f3db9a6146
LORDKINGHT <julioparra97@hotmail.com>
2020-10-22 18:08:49 -0500

    Update
    Update

--- icfesPreU/src/app/login/loginServices/login.service.ts
+++ icfesPreU/src/app/login/loginServices/login.service.ts
@@ -1,38 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-//firebase 
-import {AngularFireAuth} from '@angular/fire/auth';
+//firebase
+import { AngularFireAuth } from "@angular/fire/auth";
 
 //user
-import{user}from '../../shared/user.class';
+import { user } from "../../shared/user.class";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LoginService {
-  public isLogged: any=false;
+  public isLogged: any = false;
 
   constructor(public afAuth: AngularFireAuth) {
-  afAuth.authState.subscribe(user =>(this.isLogged= user));
-
-}
-//login
-async onLogin(user:user){
-  try {
-    return await this.afAuth.signInWithEmailAndPassword(user.email,user.password)
-  } catch (error) {
-    console.log('Error on login',error)
+    afAuth.authState.subscribe((user) => (this.isLogged = user));
   }
-}
-//register
-async onRegister (user:user){
-  try {
-    return await this.afAuth.createUserWithEmailAndPassword(
-      user.email,
-      user.password)
-  } catch (error) {
-    console.log('Error on register',error)
-    
+  // login
+  async onLogin(user: user) {
+    try {
+      return await this.afAuth.signInWithEmailAndPassword(
+        user.email,
+        user.password
+      );
+    } catch (error) {
+      console.log("Error on login", error);
+    }
   }
-}
-
+  //register
+  async onRegister(user: user) {
+    try {
+      return await this.afAuth.createUserWithEmailAndPassword(
+        user.email,
+        user.password
+      );
+    } catch (error) {
+      console.log("Error on register", error);
+    }
+  }
 }
